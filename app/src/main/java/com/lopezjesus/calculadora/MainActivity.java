c5387dd4753d938c28757fff21f974588fe4abfc
JesusLR19 <jesuseduardolopezro@gmail.com>
2023-09-13 23:55:47 -0600

    Cambio MainActivity

--- app/src/main/java/com/lopezjesus/calculadora/MainActivity.java
+++ app/src/main/java/com/lopezjesus/calculadora/MainActivity.java
@@ -9,81 +9,155 @@ import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-    TextView resultado,solucion;
-    MaterialButton btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    MaterialButton btnSuma, btnResta, btnMultip, btnDiv;
-    MaterialButton btnClear, btnPunto, btnIgual, btnPar1, btnPar2,btnDEL;
+public class MainActivity extends AppCompatActivity   {
+    TextView tvResultado;
+    Float numero1 = 0.0f, numero2 = 0.0f;
+    String operador = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultado = findViewById(R.id.resultado);
-        solucion = findViewById(R.id.solucion);
-        assignId(btn0,R.id.btn0);
-        assignId(btn1,R.id.btn1);
-        assignId(btn2,R.id.btn2);
-        assignId(btn3,R.id.btn3);
-        assignId(btn4,R.id.btn4);
-        assignId(btn5,R.id.btn5);
-        assignId(btn6,R.id.btn6);
-        assignId(btn7,R.id.btn7);
-        assignId(btn8,R.id.btn8);
-        assignId(btn9,R.id.btn9);
-        assignId(btnSuma,R.id.btnSuma);
-        assignId(btnResta,R.id.btnResta);
-        assignId(btnMultip,R.id.btnMultip);
-        assignId(btnDiv,R.id.btnDiv);
-        assignId(btnClear,R.id.btnClear);
-        assignId(btnPunto,R.id.btnPunto);
-        assignId(btnIgual,R.id.btnIgual);
-        assignId(btnPar1,R.id.btnPar1);
-        assignId(btnPar2,R.id.btnPar2);
-        assignId(btnDEL,R.id.btnDEL);
-
+        tvResultado = findViewById(R.id.tvResultado);
     }
-    void assignId(MaterialButton btn,int id){
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
-        String buttonText = button.getText().toString();
-        String dataToCalculate = solucion.getText().toString();
 
-        if(buttonText.equals("DEL")){
-            solucion.setText("");
-            resultado.setText("0");
+    public void EscribirCero(View view) {
+        String textoActual = tvResultado.getText().toString();
+
+        // Verificar si el texto actual es solo "0"
+        if (textoActual.equals("0")) {
+            // Si es "0", no hacer nada o simplemente salir del método
             return;
+        } else {
+            // Agregar "0" al texto actual
+            tvResultado.setText(textoActual + "0");
         }
-        if (buttonText.equals("=")){
-            solucion.setText(resultado.getText());
-            return;
+    }
+
+
+    public void EscribirUno(View view) {
+        if (tvResultado.getText().toString().equals("0")){
+            tvResultado.setText("0");
+        }else{
+            tvResultado.setText(tvResultado.getText() + "1");
         }
-        if (buttonText.equals("C")){
-            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+    }
+
+    public void EscribirDos(View view) {
+        if (tvResultado.getText().toString().equals("0")){
+            tvResultado.setText("0");
+        }else{
+            tvResultado.setText(tvResultado.getText() + "2");
+        }
+    }
+
+    public void EscribirTres(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
         }else {
-            dataToCalculate = dataToCalculate +buttonText;
+            tvResultado.setText(tvResultado.getText() + "3");
         }
-        solucion.setText(dataToCalculate);
-
-        String finalResult = getResult(dataToCalculate);
-        if (!finalResult.equals("Err")){
-            resultado.setText(finalResult);
-        }
-
     }
-    String getResult(String data){
-        try {
-            Context context = Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable = context.initStandardObjects();
-            String finalResult = context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-            return finalResult;
-        }catch (Exception e){
-            return "Err";
+
+    public void EscribirCuatro(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "4");
         }
+    }
+
+    public void EscribirCinco(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "5");
+        }
+    }
+
+    public void EscribirSeis(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "6");
+        }
+    }
+
+    public void EscribirSiete(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "7");
+        }
+    }
+
+    public void EscribirOcho(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "8");
+        }
+    }
+
+    public void EscribirNueve(View view) {
+        if (tvResultado.getText().toString().equals("0")) {
+            tvResultado.setText("0");
+        }else {
+            tvResultado.setText(tvResultado.getText() + "9");
+        }
+    }
+
+    public void ClearTv(View view) {
+        tvResultado.setText("");
+        numero1 = 0.0f;
+        numero2 = 0.0f;
+        operador = "";
+    }
+
+    public void Dividir(View view) {
+        operador = "/";
+        SaveOne(view);
+    }
+
+    public void Suma(View view) {
+        operador = "+";
+        SaveOne(view);
+    }
+
+    public void Multiplicar(View view) {
+        operador = "*";
+        SaveOne(view);
+    }
+
+    public void Resta(View view) {
+        operador = "-";
+        SaveOne(view);
+    }
+
+    public void BorrarDEL(View view) {
+        if (tvResultado.getText() != ""){
+            String text = tvResultado.getText().toString();
+            tvResultado.setText(text.substring(0,text.length()-1));
+        }else{
+            return;
+        }
+    }
+    public void SaveOne(View view){
+        numero1 = Float.parseFloat(tvResultado.getText().toString());
+        tvResultado.setText("");
+    }
+
+    public void Equals(View view) {
+        numero2 = Float.parseFloat(tvResultado.getText().toString());
+        Float resultado = 0.0f;
+        if (operador == "+"){
+            resultado = numero1+numero2;
+            String resultadoStr = Float.toString(resultado);
+            tvResultado.setText(resultadoStr);
+        }
+        if ()
     }
 }
+
+
+
